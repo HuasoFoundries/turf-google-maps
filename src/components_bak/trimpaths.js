46ee99b73632cf2479bdd96bf92c191692c4be28
Amenadiel <amenadiel@gmail.com>
2017-11-21 12:08:33 -0300

    building from monolythic turf

---
+++ src/components_bak/trimpaths.js
@@ -0,0 +1,255 @@
+import {
+	default as _filter
+} from 'lodash-es/filter.js';
+import {
+	default as _max
+} from 'lodash-es/max.js';
+import {
+	default as _min
+} from 'lodash-es/min.js';
+
+import turk_kinks from '@turf/kinks';
+
+import turf_line_slice from '@turf/line-slice';
+
+import turf_line_intersect from '@turf/line-intersect';
+
+import {
+	point as turf_point,
+	lineString as turf_linestring,
+	featureCollection as turf_featurecollection
+} from '@turf/helpers';
+
+
+import {
+	debug,
+	polylineToFeatureLinestring
+} from './utils.js';
+
+import {
+	toCoords,
+	toLatLngs
+} from './coords_to_latlng.js';
+
+import {
+	default as _isEqual
+}
+from 'lodash-es/isEqual.js';
+
+
+/**
+ * Takes two coordinates and returns the distance between them, in degrees
+ * @param  {Array<number>} coord1 An array indicating a coordinate [lng, lat]
+ * @param  {Array<number>} coord2 An array indicating a coordinate [lng, lat]
+ * @return {number}        the distance between the points, in degrees 
+ */
+function diffCoords(coord1, coord2) {
+	var vector = [Math.abs(coord1[0] - coord2[0]), Math.abs(coord1[1] - coord2[1])];
+	return Math.sqrt(Math.pow(vector[0], 2) + Math.pow(vector[1], 2));
+}
+
+/**
+ * Determina si dos lineas se intersectan
+ * @param  {Array.<number>} line1Start [description]
+ * @param  {Array.<number>} line1End   [description]
+ * @param  {Array.<number>} line2Start [description]
+ * @param  {Array.<number>} line2End   [description]
+ * @param {boolean} useOldMethod if true, use old method instead of turf_line_intersect 
+ * @return {Array}             [description]
+ */
+function lineIntersects(line1Start, line1End, line2Start, line2End, useOldMethod) {
+
+	if (!useOldMethod) {
+		var line1 = turf_linestring([line1Start, line1End]),
+			line2 = turf_linestring([line2Start, line2End]),
+			intersectionFC = turf_line_intersect(line1, line2);
+
+		if (intersectionFC.features.length) {
+			var intersection = intersectionFC.features[0].geometry.coordinates;
+			intersection[0] = Math.round(intersection[0] * 100000000) / 100000000;
+			intersection[1] = Math.round(intersection[1] * 100000000) / 100000000;
+			return intersection;
+		} else {
+			return false;
+		}
+	}
+	var line1StartX = line1Start[0],
+		line1StartY = line1Start[1],
+		line1EndX = line1End[0],
+		line1EndY = line1End[1],
+		line2StartX = line2Start[0],
+		line2StartY = line2Start[1],
+		line2EndX = line2End[0],
+		line2EndY = line2End[1];
+	// if the lines intersect, the result contains the x and y of the intersection
+	// (treating the lines as infinite) and booleans for whether line segment 1 or line segment 2 contain the point
+	var denominator, a, b, numerator1, numerator2,
+		result = {
+			x: null,
+			y: null,
+			onLine1: false,
+			onLine2: false
+		};
+	denominator = ((line2EndY - line2StartY) * (line1EndX - line1StartX)) - ((line2EndX - line2StartX) * (line1EndY - line1StartY));
+	if (denominator === 0) {
+		if (result.x !== null && result.y !== null) {
+			return result;
+		} else {
+			return false;
+		}
+	}
+	a = line1StartY - line2StartY;
+	b = line1StartX - line2StartX;
+	numerator1 = ((line2EndX - line2StartX) * a) - ((line2EndY - line2StartY) * b);
+	numerator2 = ((line1EndX - line1StartX) * a) - ((line1EndY - line1StartY) * b);
+	a = numerator1 / denominator;
+	b = numerator2 / denominator;
+
+	// if we cast these lines infinitely in both directions, they intersect here:
+	result.x = line1StartX + (a * (line1EndX - line1StartX));
+	result.y = line1StartY + (a * (line1EndY - line1StartY));
+
+	// if line1 is a segment and line2 is infinite, they intersect if:
+	if (a >= 0 && a <= 1) {
+		result.onLine1 = true;
+	}
+	// if line2 is a segment and line1 is infinite, they intersect if:
+	if (b >= 0 && b <= 1) {
+		result.onLine2 = true;
+	}
+	// if line1 and line2 are segments, they intersect if both of the above are true
+	if (result.onLine1 && result.onLine2) {
+		result.x = Math.round(result.x * 100000000) / 100000000;
+		result.y = Math.round(result.y * 100000000) / 100000000;
+
+		return [result.x, result.y];
+	} else {
+		return false;
+	}
+}
+
+/**
+ * Takes two rings and finds their instersection points. If the rings are the same, the second ring is iterated skipping points already checked in the first one
+ * @param  {Array.Array<number>} ring1 Array of coordinates [lng, lat]
+ * @param  {Array.Array<number>} ring1 Array of coordinates [lng, lat]
+ * @param {boolean} useOldMethod if true, use old method instead of turf_line_intersect 
+ * @return {Object}       an object containing
+ */
+function traverseRings(ring1, ring2, useOldMethod) {
+	var intersections = turf_featurecollection([]);
+
+	var samering = false,
+		consecutive = false;
+	if (_isEqual(ring1, ring2)) {
+		samering = true;
+	}
+	for (var i = 0; i < ring1.length - 1; i++) {
+		var startK = samering ? i : 0;
+		for (var k = startK; k < ring2.length - 1; k++) {
+			// don't check adjacent sides of a given ring, since of course they intersect in a vertex.
+			if (ring1 === ring2 && (Math.abs(i - k) === 1 || Math.abs(i - k) === ring1.length - 2)) {
+				continue;
+			}
+
+
+			var intersection = lineIntersects(
+				ring1[i],
+				ring1[i + 1],
+				ring2[k],
+				ring2[k + 1],
+				useOldMethod
+			);
+			if (!intersection) {
+				continue;
+			}
+
+			// si son lineas consecutivas no quiero detectar el límite entre ambas
+			if ((diffCoords(intersection, ring1[0]) < 0.000005 || diffCoords(intersection, ring1[ring1.length - 1]) < 0.000005) &&
+				(diffCoords(intersection, ring2[0]) < 0.000005 || diffCoords(intersection, ring2[ring2.length - 1]) < 0.000005)) {
+				continue;
+			}
+
+			//debug('intersection at',
+			// intersection,
+			//diffCoords(intersection, ring2[0]),
+			//diffCoords(intersection, ring1[ring1.length - 1]));
+			var FeatureIntersection = turf_point([intersection[0], intersection[1]]);
+			FeatureIntersection.properties = {
+				position1: i,
+				position2: k
+			};
+			intersections.features.push(FeatureIntersection);
+
+		}
+	}
+	return intersections;
+};
+
+
+/**
+ * Finds the {@link Point|points} where two {@link LineString|linestrings} intersect each other
+ * @param  {Array.<google.maps.LatLng>} arrayLatLng1 array de posiciones {@link google.maps.LatLng}
+ * @param  {Array.<google.maps.LatLng>} arrayLatLng2 array de posiciones {@link google.maps.LatLng}
+ * @param {boolean} useOldMethod if true,use old method instead of turf_line_intersect 
+ * @return {Array}        an array with [line1 trimmed at intersection,line2 trimmed at intersection,intersection ] 
+ */
+function trimPaths(arrayLatLng1, arrayLatLng2, useOldMethod) {
+
+	var ring1 = toCoords(arrayLatLng1); // googleGeom1.geometry.coordinates;
+	var ring2 = toCoords(arrayLatLng2); // googleGeom2.geometry.coordinates;
+
+
+	var intersections = traverseRings(ring1, ring2, useOldMethod);
+
+	if (intersections.features.length > 0) {
+
+		var line1 = turf_linestring(ring1);
+		var line2 = turf_linestring(ring2);
+		var line1Start = turf_point(ring1[0]);
+		var line2End = turf_point(ring2.slice(-1)[0]);
+		var sliced1, sliced2;
+
+		// The first segment of the first ring with a kink
+		var first_segment_with_kinks = _min(intersections.features, function (kink) {
+			return kink.properties.position1;
+		});
+		//console.log('first_segment_with_kinks', JSON.stringify(first_segment_with_kinks));
+
+		// All the intersections which belong to the first segment with a kink of the first ring
+		var kinks_in_first_segment = _filter(intersections.features, function (kink) {
+			return kink.properties.position1 === first_segment_with_kinks.properties.position1;
+		});
+
+		// Among the kinks in the first segment, which one happens further along the ring2
+		var chosenIntersection = _max(kinks_in_first_segment, function (kink) {
+			return kink.properties.position2;
+		});
+
+		var intersectLatLng = toLatLngs([chosenIntersection.geometry.coordinates])[0];
+
+		// if the first intersection happens in the first segment of line1
+		// then we don't slice it
+		if (chosenIntersection.properties.position1 === 0) {
+			sliced1 = line1;
+		} else {
+			sliced1 = turf_line_slice(line1Start, chosenIntersection, line1);
+		}
+
+		// if the first intersection happens after the last segment of line2
+		// then we don't slice it
+		if (chosenIntersection.properties.position2 >= (ring2.length - 1)) {
+			sliced2 = line2;
+		} else {
+			sliced2 = turf_line_slice(chosenIntersection, line2End, line2);
+		}
+
+
+		return [toLatLngs(sliced1.geometry.coordinates), toLatLngs(sliced2.geometry.coordinates), intersectLatLng];
+	}
+	return [];
+
+};
+
+export {
+	trimPaths
+}
---
+++ src/components_bak/utils.js
@@ -0,0 +1,225 @@
+import {
+    Wicket
+} from './wicket_helper.js';
+
+
+import {
+    default as _map
+} from 'lodash-es/map.js';
+
+import {
+    default as _each
+} from 'lodash-es/forEach.js';
+
+import {
+    toCoords
+} from './coords_to_latlng.js';
+
+import {
+    default as _size
+} from 'lodash-es/size.js';
+
+import {
+    default as _reduce
+}
+from 'lodash-es/reduce.js';
+
+
+import {
+    lineString as turf_linestring
+} from '@turf/helpers';
+
+
+var debug = console.debug.bind(console, '%c turfHelper' + ':', "color:#00CC00;font-weight:bold;"),
+    warn = console.debug.bind(console, '%c turfHelper' + ':', "color:orange;font-weight:bold;");
+
+
+/**
+ * Transforma un array de gmaps.LatLng en un Feature.Polygon
+ * @param  {Array.<google.maps.LatLng>} LatLngArray [description]
+ * @return {Feature.<Polygon>}             [description]
+ */
+function arrayToFeaturePolygon(LatLngArray) {
+
+    var vertices = toCoords(LatLngArray, true);
+
+    return {
+        type: "Feature",
+        properties: {},
+        geometry: {
+            type: "Polygon",
+
+            coordinates: [vertices]
+        }
+    };
+}
+
+/**
+ * Transforms a {@link google.maps.LatLng} or {@link google.maps.LatLngLiteral} into a {@link Feature.<Point>}
+ * @param  {google.maps.LatLng|google.maps.LatLngLiteral|Array.<Number>} LatLng a coordinate to transform
+ * @return {Feature.<Point>} a Point type Feature
+ */
+function latlngToFeaturePoint(LatLng) {
+    var coords = toCoord([LatLng])[0],
+        feature = {
+            type: "Feature",
+            geometry: {
+                type: "Point",
+                coordinates: coords
+            }
+        };
+
+    return feature;
+}
+
+/**
+ * Transforms a {@link google.maps.Marker} to a {@link Feature<Point>}
+ * @param  {google.maps.Marker} marker  - marker object to transform
+ * @return {Feature<Point>}    output Feature
+ */
+function markerToFeaturePoint(marker) {
+    if (!marker.getPosition || typeof marker.getPosition !== 'function') {
+        throw new Error('input object does not have a getPosition method');
+    }
+    var position = marker.getPosition(),
+        Feature = {
+            type: "Feature",
+            properties: {},
+            geometry: {
+                type: "Point",
+                coordinates: [position.lng(), position.lat()]
+            }
+        };
+
+    return Feature;
+};
+
+
+/**
+ * [polylineToFeatureLinestring description]
+ * @param  {Array.<google.maps.LatLng>|google.maps.Polyline} objeto array of positions or a google.maps.Polyline
+ * @return {Feature.<LineString>}          [description]
+ */
+function polylineToFeatureLinestring(objeto) {
+    var vertices
+    if (objeto instanceof google.maps.Polyline) {
+        vertices = toCoords(objeto.getPath().getArray());
+    } else {
+        vertices = toCoords(objeto);
+    }
+
+    return turf_linestring(vertices);
+}
+
+
+/**
+ * Receives an object and returns a GeoJson Feature of type Polygon
+ * @param  {google.maps.Polygon|Array.<google.maps.LatLng>|Feature.Polygon|Geometry} object object to transform into a Feature.Polygon
+ * @return {Feature.Polygon}        [description]
+ */
+function polygonToFeaturePolygon(object) {
+    var ring, polygonFeature;
+
+    if (object.type === 'Feature') {
+        polygonFeature = object;
+
+    } else if (object instanceof google.maps.Polygon) {
+
+        object = object.getPath().getArray();
+        ring = toCoords(object, true);
+        polygonFeature = arrayToFeaturePolygon(ring);
+
+    } else if (!!(object && object.constructor === Array)) {
+
+        ring = toCoords(object, true);
+        polygonFeature = arrayToFeaturePolygon(ring);
+
+    } else if (object.geometry) {
+
+        polygonFeature = {
+            type: "Feature",
+            properties: {},
+            geometry: object.geometry
+        };
+
+    } else {
+        throw new Error('object is not a Feature, google.maps.Polygon nor an array of google.maps.LatLng');
+    }
+
+    polygonFeature.properties = {};
+
+
+    return polygonFeature;
+}
+
+
+/**
+ * Transforma un array de gmaps.LatLng en un featurecollection geoJson
+ * donde cada Feature es un punto del array de entrada
+ * @param  {Array<google.maps.LatLng>|google.maps.MVCArray} latLngArray array de posiciones {@link google.maps.LatLng}
+ * @return {FeatureCollection}             geojson FeatureCollection
+ */
+function arrayToFeaturePoints(latLngArray) {
+
+
+    var FeatureCollection = {
+        "type": "FeatureCollection",
+        "features": []
+    };
+    latLngArray.forEach(function (latLng) {
+        var Feature = {
+            type: "Feature",
+            geometry: {
+                type: "Point",
+                coordinates: toCoords([latLng])[0]
+            }
+        };
+        FeatureCollection.features.push(Feature);
+    });
+
+    return FeatureCollection;
+
+}
+
+
+/**
+ * Convierte un gmaps.Polygon en un FeatureCollection de puntos
+ * @param  {google.maps.Polygon} polygon [description]
+ * @return {FeatureCollection.<Point>}         [description]
+ */
+function polygonToFeaturePolygonCollection(polygon) {
+    var geojsonPolygon = polygonToFeaturePolygon(polygon);
+
+    var vertexToFeature = function (vertex) {
+        return {
+            type: "Feature",
+            geometry: {
+                type: "Point",
+                coordinates: vertex
+            }
+        };
+    };
+
+    var FeatureCollection = {
+        type: "FeatureCollection",
+        features: _map(geojsonPolygon.coordinates[0], vertexToFeature)
+    };
+
+    FeatureCollection.features.push(vertexToFeature(geojsonPolygon.coordinates[0][0]));
+
+    return FeatureCollection;
+}
+
+
+export {
+    debug,
+    warn,
+    centroid,
+    arrayToFeaturePolygon,
+    polygonToFeaturePolygonCollection,
+    arrayToFeaturePoints,
+    markerToFeaturePoint,
+    latlngToFeaturePoint,
+    polygonToFeaturePolygon,
+    polylineToFeatureLinestring
+};
